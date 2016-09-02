563dc23648aeda9495dfcf38edc37e924da306f0
Mikhail Davydenko <mikhail.davidenko@nixsolutions.com>
2016-09-02 13:08:16 +0300

    Add base entities and IConverter interface

---
+++ BookMe/BookMe.ShareProint.Data/Converters/Abstract/IConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMe.ShareProint.Data.Converters.Abstract
+{
+    public interface IConverter<in TInput, out TOutput>
+    {
+        TOutput Convert(TInput value);
+
+        IEnumerable<TOutput> Convert(IEnumerable<TInput> values);
+    }
+}
