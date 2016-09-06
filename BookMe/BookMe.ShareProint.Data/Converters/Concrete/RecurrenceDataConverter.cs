d565c3c2c1ad3371d2dd1ba61854f67f694061cd
Mikhail Davydenko <mikhail.davidenko@nixsolutions.com>
2016-09-06 11:48:18 +0300

    Add empty RecurenceDataConverter

---
+++ BookMe/BookMe.ShareProint.Data/Converters/Concrete/RecurrenceDataConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookMe.Core.Models.Recurrence;
+using BookMe.ShareProint.Data.Converters.Abstract;
+
+namespace BookMe.ShareProint.Data.Converters.Concrete
+{
+    public class RecurrenceDataConverter : IConverter<string, RecurrenceData>
+    {
+        public RecurrenceData Convert(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RecurrenceData> Convert(IEnumerable<string> values)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
