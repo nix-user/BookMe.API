e373efebf8f8e03b1ea67cf959cb41cf0f58b7f7
Mikhail Davydenko <mikhail.davidenko@nixsolutions.com>
2016-09-06 12:36:42 +0300

    Add DailyPattern class

---
+++ BookMe/BookMe.Core/Models/Recurrence/DailyPattern.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMe.Core.Models.Recurrence
+{
+    public sealed class DailyPattern : IntervalPattern
+    {
+        public DailyPattern(DateTime startDate, int interval)
+        {
+            this.StartDate = startDate;
+            this.Interval = interval;
+        }
+
+        public override Interval GetBusyInterval(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
