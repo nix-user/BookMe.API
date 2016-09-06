7a69a089ea68d75626d74ccb2b6117725e609d68
Mikhail Davydenko <mikhail.davidenko@nixsolutions.com>
2016-09-06 18:01:30 +0300

    Implement ReservationConverter

--- BookMe/BookMe.Core/Models/Reservation.cs
+++ BookMe/BookMe.Core/Models/Reservation.cs
@@ -8,5 +8,20 @@ namespace BookMe.Core.Models
 {
     public class Reservation : BaseEntity
     {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime EventDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public bool IsRecurrence { get; set; }
+
+        public long? ResourceId { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public string OwnerName { get; set; }
     }
 }
---
+++ BookMe/BookMe.ShareProint.Data/Converters/Concrete/ReservationConverter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookMe.Core.Models;
+using BookMe.ShareProint.Data.Converters.Abstract;
+using Microsoft.SharePoint.Client;
+
+namespace BookMe.ShareProint.Data.Converters.Concrete
+{
+    public class ReservationConverter : IConverter<IDictionary<string, object>, Reservation>
+    {
+        private const string IdKey = "ID";
+        private const string TitleKey = "Title";
+        private const string DescriptionKey = "Description";
+        private const string FacilitiesKey = "Facilities";
+        private const string EventDateKey = "EventDate";
+        private const string EndDateKey = "EndDate";
+        private const string DurationKey = "Duration";
+        private const string IsRecurrenceKey = "fRecurrence";
+        private const string AuthorKey = "Author";
+
+        public Reservation Convert(IDictionary<string, object> value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            var reservation = new Reservation()
+            {
+                Id = int.Parse(value[IdKey].ToString()),
+                Title = value[TitleKey].ToString(),
+                Description = value[DescriptionKey]?.ToString(),
+                ResourceId = (value[FacilitiesKey] as IList<FieldLookupValue>)?[0].LookupId,
+                EventDate = DateTime.Parse(value[EventDateKey].ToString()),
+                EndDate = DateTime.Parse(value[EndDateKey].ToString()),
+                Duration = TimeSpan.FromSeconds(long.Parse(value[DurationKey].ToString())),
+                IsRecurrence = bool.Parse(value[IsRecurrenceKey].ToString()),
+                OwnerName = (value[AuthorKey] as FieldUserValue)?.LookupValue
+            };
+
+            return reservation;
+        }
+
+        public IEnumerable<Reservation> Convert(IEnumerable<IDictionary<string, object>> values)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            return values.Select(this.Convert);
+        }
+    }
+}
