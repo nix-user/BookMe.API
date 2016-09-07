a1fb5925d61bf5a0d749c565aa1b0096d8296c5e
Sergey Yushkin <sergey.yushkin@nixsolutions.com>
2016-09-07 12:16:12 +0300

    rewritten reservation controller test

--- BookMe/BookMe.UnitTests/Controllers/ReservationControllerTests.cs
+++ BookMe/BookMe.UnitTests/Controllers/ReservationControllerTests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using BookMe.BusinessLogic.DTO;
 using BookMe.BusinessLogic.Interfaces.SharePoint;
 using BookMe.BusinessLogic.OperationResult;
+using BookMe.Infrastructure.MapperConfiguration;
 using BookMe.WebApi.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -17,24 +18,31 @@ namespace BookMe.UnitTests.Controllers
     {
         private ReservationController reservationController;
         private Mock<ISharePointReservationService> sharePointReservationServiceMock;
+        private List<ReservationDTO> reservationsList = new List<ReservationDTO>();
 
         [TestInitialize]
         public void Init()
         {
+            AutoMapperConfiguration.Configure();
             this.sharePointReservationServiceMock = new Mock<ISharePointReservationService>();
-            this.sharePointReservationServiceMock.Setup(m => m.GetUserActiveReservations(It.IsAny<string>()))
-                .Returns(new OperationResult<IEnumerable<ReservationDTO>>());
+            for (int i = 0; i < 10; i++)
+            {
+                this.reservationsList.Add(new ReservationDTO());
+            }
+            var userActiveReservationResult = new OperationResult<IEnumerable<ReservationDTO>> { IsSuccessful = true, Result = reservationsList };
+            this.sharePointReservationServiceMock.Setup(m => m.GetUserActiveReservations(It.IsAny<string>())).Returns(userActiveReservationResult);
             this.reservationController = new ReservationController(this.sharePointReservationServiceMock.Object);
         }
 
         [TestMethod]
-        public void GetUserReservations_Should_Call_ReservationService_GetUserActiveReservations()
+        public void GetUserReservations_Should_Return_All_Reservations()
         {
             //act
-            this.reservationController.GetUserReservations(string.Empty);
+            var userReservations = this.reservationController.GetUserReservations(string.Empty);
 
             //assert
             this.sharePointReservationServiceMock.Verify(m => m.GetUserActiveReservations(It.IsAny<string>()), Times.Once);
+            Assert.AreEqual(this.reservationsList.Count, userReservations.Count());
         }
     }
 }
