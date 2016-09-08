62214d1bb1cebc5fdc44e21de61504783a3f922a
Mikhail Davydenko <mikhail.davidenko@nixsolutions.com>
2016-09-08 15:13:04 +0300

    Refactor DailyPattern

--- BookMe/BookMe.Core/Models/Recurrence/DailyPattern.cs
+++ BookMe/BookMe.Core/Models/Recurrence/DailyPattern.cs
@@ -15,27 +15,18 @@ namespace BookMe.Core.Models.Recurrence
         {
             if (this.Interval == null)
             {
-                var daysIntersect = this.DaysOfTheWeek.Intersect(this.DaysOThefWeekByDayOfWeek[date.DayOfWeek]);
-                if (!daysIntersect.Any())
-                {
-                    throw new Exception("shit happend bro");
-                }
-                else if (this.NumberOfOccurrences != null)
+                if (this.NumberOfOccurrences != null)
                 {
                     var countOfInstances = this.EachDay(this.StartDate, SystemTime.Now())
                         .Count(item => this.IsDateInDaysOfTheWeek(item, this.DaysOfTheWeek));
 
-                    return countOfInstances < this.NumberOfOccurrences;
-                }
-                else
-                {
-                    return this.EndDate == null || this.EndDate > SystemTime.Now();
+                    return countOfInstances <= this.NumberOfOccurrences;
                 }
             }
             else
             {
                 var totalDays = (int)(date - this.StartDate).TotalDays;
-                if ((totalDays - 1) % 2 != 0)
+                if (totalDays % this.Interval != 0)
                 {
                     return false;
                 }
@@ -43,18 +34,16 @@ namespace BookMe.Core.Models.Recurrence
                 {
                     return totalDays < this.Interval * this.NumberOfOccurrences;
                 }
-                else
-                {
-                    return this.EndDate == null || this.EndDate > SystemTime.Now();
-                }
             }
+
+            return this.EndDate == null || this.EndDate > SystemTime.Now();
         }
 
         private bool IsDateInDaysOfTheWeek(DateTime date, IEnumerable<DayOfTheWeek> daysOfTheWeek)
         {
             foreach (var item in daysOfTheWeek)
             {
-                if (this.DaysOfTheWeek.Intersect(this.DaysOThefWeekByDayOfWeek[date.DayOfWeek]).Any())
+                if (this.DaysOThefWeekByDayOfWeek[date.DayOfWeek].Contains(item))
                 {
                     return true;
                 }
