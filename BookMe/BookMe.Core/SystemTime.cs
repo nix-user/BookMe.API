d53d65d758dd7b875f63643a5aa85a7f6a947a14
Mikhail Davydenko <mikhail.davidenko@nixsolutions.com>
2016-09-08 14:03:57 +0300

    Add SystemTime class to be able to mock Datetime.Now

---
+++ BookMe/BookMe.Core/SystemTime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BookMe.Core
+{
+    public static class SystemTime
+    {
+        private static Func<DateTime> now = () => DateTime.Now;
+
+        public static Func<DateTime> Now
+        {
+            get { return now; }
+            set { now = value; }
+        }
+    }
+}
