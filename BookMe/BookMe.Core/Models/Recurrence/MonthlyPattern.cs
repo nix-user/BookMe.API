8086384003f6b646410fded1401a8f1f8dd1d422
Mikhail Davydenko <mikhail.davidenko@nixsolutions.com>
2016-09-09 11:43:36 +0300

    Implement monthly pattern

--- BookMe/BookMe.Core/Models/Recurrence/MonthlyPattern.cs
+++ BookMe/BookMe.Core/Models/Recurrence/MonthlyPattern.cs
@@ -12,7 +12,48 @@ namespace BookMe.Core.Models.Recurrence
 
         public override bool IsBusyInDate(DateTime date)
         {
-            throw new NotImplementedException();
+            var totalMonth = this.CalculateMonthCount(this.StartDate, date);
+            var isDayOfMonthRight = this.DayOfMonth == date.Day;
+            if (totalMonth % this.Interval != 0)
+            {
+                return false;
+            }
+            else
+            {
+                if (this.NumberOfOccurrences != null)
+                {
+                    var countOfInstances = this.CalculateInstancesCount(this.StartDate, date);
+                    if (countOfInstances <= this.NumberOfOccurrences)
+                    {
+                        return isDayOfMonthRight;
+                    }
+
+                    return false;
+                }
+            }
+
+            return (this.EndDate == null || this.EndDate > date) && isDayOfMonthRight;
+        }
+
+        private int CalculateMonthCount(DateTime from, DateTime to)
+        {
+            return (to.Month - from.Month) + 12 * (to.Year - from.Year);
+        }
+
+        private int CalculateInstancesCount(DateTime from, DateTime to)
+        {
+            var totalMonth = this.CalculateMonthCount(from, to);
+            if (from.Day > this.DayOfMonth)
+            {
+                totalMonth--;
+            }
+
+            if (to.Day < this.DayOfMonth)
+            {
+                totalMonth--;
+            }
+
+            return totalMonth;
         }
     }
 }
