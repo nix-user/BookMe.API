de75f3eddd73d88cf20bbf8dcaed0d5e4f145c8e
bogdan.dima96 <dmitriy.bogdan@nixsolutions.com>
2016-09-12 13:31:48 +0300

    fix bugs

---
+++ BookMe/BookMe.WebApi/MapperProfiles/ReservationDTOProfile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using BookMe.BusinessLogic.DTO;
+using BookMe.WebApi.Models;
+
+namespace BookMe.WebApi.MapperProfiles
+{
+    public class ReservationDTOProfile : Profile
+    {
+        public ReservationDTOProfile()
+        {
+            this.CreateMap<ReservationModel, ReservationDTO>()
+                .ForMember(nameof(ReservationDTO.OwnerName), opt => opt.MapFrom(reservation => reservation.Author))
+                .ForMember(nameof(ReservationDTO.EventDate), opt => opt.MapFrom(reservation => reservation.From))
+                .ForMember(nameof(ReservationDTO.EndDate), opt => opt.MapFrom(reservation => reservation.To))
+                .ForMember(nameof(ReservationDTO.IsRecurrence), opt => opt.MapFrom(reservation => reservation.IsRecursive));
+
+            this.CreateMap<ReservationDTO, ReservationModel>()
+                .ForMember(nameof(ReservationModel.Author), opt => opt.MapFrom(reservation => reservation.OwnerName))
+                .ForMember(nameof(ReservationModel.From), opt => opt.MapFrom(reservation => reservation.EventDate))
+                .ForMember(nameof(ReservationModel.To), opt => opt.MapFrom(reservation => reservation.EndDate))
+                .ForMember(nameof(ReservationModel.IsRecursive), opt => opt.MapFrom(reservation => reservation.IsRecurrence))
+                .ForMember(nameof(ReservationModel.Room), opt => opt.MapFrom(reservation => reservation.Resource));
+        }
+    }
+}
