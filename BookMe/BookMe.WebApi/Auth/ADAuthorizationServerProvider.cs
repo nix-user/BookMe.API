5beeb6aa01da5974fd9c86985ffdcf2e082177bc
Mikhail Davydenko <mikhail.davidenko@nixsolutions.com>
2016-09-14 11:42:06 +0300

    Implement API authentication

---
+++ BookMe/BookMe.WebApi/Auth/ADAuthorizationServerProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Owin.Security.OAuth;
+
+namespace BookMe.WebApi.Auth
+{
+    public class ADAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        private const string InvalidGrantKey = "invalid_grant";
+        private const string InvalidGrantMessage = "The user name or password is incorrect";
+
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            using (var principalContext = new PrincipalContext(ContextType.Domain))
+            {
+                var isValid = principalContext.ValidateCredentials(context.UserName, context.Password);
+                if (!isValid)
+                {
+                    context.SetError(InvalidGrantKey, InvalidGrantMessage);
+                    return;
+                }
+
+                var userPrincipal = UserPrincipal.FindByIdentity(principalContext, context.UserName);
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim(ClaimTypes.Name, userPrincipal.Name));
+                context.Validated(identity);
+            }
+        }
+    }
+}
--- BookMe/BookMe.WebApi/Startup.cs
+++ BookMe/BookMe.WebApi/Startup.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Http;
+using BookMe.WebApi.Auth;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 
 [assembly: OwinStartup(typeof(BookMe.WebApi.Startup))]
@@ -12,6 +15,29 @@ namespace BookMe.WebApi
     {
         public void Configuration(IAppBuilder app)
         {
+            var config = new HttpConfiguration();
+
+            this.ConfigureOAuth(app);
+
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            const int ExpirationTimeInDays = 365 * 100;
+
+            var oAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(ExpirationTimeInDays),
+                Provider = new ADAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(oAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
         }
     }
 }
