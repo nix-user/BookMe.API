96ed5438de31430a978ee98c00c0493b1786c8b9
Sergey Yushkin <sergey.yushkin@nixsolutions.com>
2016-09-15 09:55:57 +0300

    added comment to event type  enumeration that explains why there's no event type that equals to two

--- BookMe/BookMe.Core/Enums/EventType.cs
+++ BookMe/BookMe.Core/Enums/EventType.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace BookMe.Core.Enums
 {
+    // There's no eventType that equals to two because there is no such event type on SharePoint side
     public enum EventType
     {
         Regular = 0,
